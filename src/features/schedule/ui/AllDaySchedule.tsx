2a046147d7acd4267af2d14b4bd1d68afff5e8f2
kimjungkwang1 <blzbtn0923@gmail.com>
2024-05-08 16:47:49 +0900

    ✨Feat :  add detail schedule
    Change-Id: I87f76f8fff559b03dbc6d57fad37de402d36fcb0

--- src/features/schedule/ui/AllDaySchedule.tsx
+++ src/features/schedule/ui/AllDaySchedule.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { Color } from "@/shared/lib/styles/color";
-type color = "blue" | "green" | "red" | "yellow";
+import { DetailSchedule } from "@/features/schedule/index";
+type color = "blue" | "green" | "orange" | "yellow";
 
 // 종일 일정 컴포넌트의 props
 interface IAllDayScheduleProps {
@@ -12,10 +13,29 @@ interface IAllDayScheduleProps {
 // 종일 일정 컴포넌트
 export function AllDaySchedule({ title, width }: IAllDayScheduleProps) {
   const [showDetails, setShowDetails] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>(null);
+  // 외부영역 클릭시 더보기 일정 닫기
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setShowDetails(false);
+      }
+    };
+    if (showDetails) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showDetails]);
+
   return (
-    <Layout width={width} color="blue" onClick={() => setShowDetails(!showDetails)}>
-      {title}
-    </Layout>
+    <MainLayout onClick={(e) => e.stopPropagation()}>
+      <Layout width={width} color="blue" onClick={() => setShowDetails((prev) => !prev)}>
+        <TextDiv>{title}</TextDiv>
+      </Layout>
+      {showDetails && <DetailSchedule close={() => setShowDetails(false)}></DetailSchedule>}
+    </MainLayout>
   );
 }
 // 종일 일정 컴포넌트 스타일
@@ -25,7 +45,27 @@ const Layout = styled.div<{ color: color; width: number }>`
   border-radius: 5px;
   color: white;
   text-align: left;
-  padding: 0 5px;
+  padding: 0px 5px;
   font-size: small;
   position: relative;
+  &:hover {
+    background-color: lightcoral;
+  }
+  margin: 1px 0px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+const TextDiv = styled.div`
+  width: 100px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+const MainLayout = styled.div`
+  width: 100%;
+  text-align: left;
+  font-size: small;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `;
