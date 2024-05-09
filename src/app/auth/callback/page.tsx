27e7cb1f05709d4c6494173270ca3f73f034f5e8
card0923 <card0293@naver.com>
2024-05-09 16:54:36 +0900

    🚑Hotfix :  fix client-side exception
    Change-Id: Ia5d0bf81d4a88b3d1f9cfc582cd0573b9136aa8b

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,9 +1,29 @@
 "use client";
 import React, { useEffect } from "react";
-import LoginResponse from "./LoginResponse";
+import { useRouter } from "next/router";
 export default function Page() {
+  const router = useRouter();
   useEffect(() => {
-    LoginResponse();
-  }, []);
+    const refreshToken = Array.isArray(router.query["refresh-token"])
+      ? router.query["refresh-token"][0]
+      : router.query["refresh-token"] || "";
+    const accessToken = Array.isArray(router.query["access-token"])
+      ? router.query["access-token"][0]
+      : router.query["access-token"] || "";
+    const expiresIn = Array.isArray(router.query["expires_in"])
+      ? router.query["expires_in"][0]
+      : router.query["expires_in"] || "";
+    sessionStorage.setItem("refreshToken", refreshToken || "");
+    sessionStorage.setItem("accessToken", accessToken || "");
+    sessionStorage.setItem("expiresIn", expiresIn || "");
+    if (expiresIn) {
+      const currentTime = new Date();
+      const expiresAt = new Date(
+        currentTime.getTime() + parseInt(expiresIn, 10) * 1000 - 10 * 60 * 1000
+      );
+      sessionStorage.setItem("expiresAt", expiresAt.toISOString());
+    }
+    router.push("/");
+  }, [router.query]);
   return <div></div>;
 }
