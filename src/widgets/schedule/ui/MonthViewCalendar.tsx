141991790578ed66d271bef8779369c5b322d0e3
card0923 <card0293@naver.com>
2024-05-14 11:10:33 +0900

    🎨Design :  add color
    Change-Id: I0c59e52939b7b23e36886e0e92853383298942fa

--- src/widgets/schedule/ui/MonthViewCalendar.tsx
+++ src/widgets/schedule/ui/MonthViewCalendar.tsx
@@ -16,98 +16,181 @@ import {
   SeparateSchedule,
 } from "@/features/schedule/index";
 import { Week } from "@/features/schedule/index";
+import { schedule } from "@/widgets/schedule/model/type";
 
 // 월별 캘린더의 props
 interface IMonthViewCalendarProps {
   selectedDate: Date;
-  scheduleList: any;
+  scheduleList: schedule[];
+  triggerReload: () => void;
 }
 // 월별 캘린더 컴포넌트
-export function MonthViewCalendar({ selectedDate, scheduleList }: IMonthViewCalendarProps) {
+export function MonthViewCalendar({
+  selectedDate,
+  scheduleList,
+  triggerReload,
+}: IMonthViewCalendarProps) {
   const firstDayOfMonth = startOfMonth(selectedDate); // 선택된 달의 첫날
   const lastDayOfMonth = endOfMonth(selectedDate); // 선택된 달의 마지막날
-  const startDate = startOfWeek(firstDayOfMonth); // 선택된 달의 첫주의 첫날
-  const endDate = endOfWeek(lastDayOfMonth); // 선택된 달의 마지막주의 마지막날
-  let currentDate = startDate; // 현재 날짜
+  const cStartDate = startOfWeek(firstDayOfMonth); // 선택된 달의 첫주의 첫날
+  const cEndDate = endOfWeek(lastDayOfMonth); // 선택된 달의 마지막주의 마지막날
+  let currentDate = cStartDate; // 현재 날짜
   // 날짜 배열 선언
   const dates: { [key: string]: React.ReactNode[] } = {};
   // 날짜 배열 채우기
-  while (currentDate <= endDate) {
+  while (currentDate <= cEndDate) {
     dates[format(currentDate, "yyyy-MM-dd")] = [];
     currentDate = addDays(currentDate, 1);
   }
+  console.log(scheduleList);
   scheduleList.sort(
-    (a: any, b: any) =>
-      differenceInCalendarDays(b.endDateTime, b.startDateTime) -
-      differenceInCalendarDays(a.endDateTime, a.startDateTime)
+    (a: schedule, b: schedule) =>
+      differenceInCalendarDays(new Date(b.endDatetime), new Date(b.startDatetime)) -
+      differenceInCalendarDays(new Date(a.endDatetime), new Date(a.startDatetime))
   );
   // api 연결전 임시 조건 삭제 예정
-  if (selectedDate.getMonth() === 4) {
-    // 일정별 날짜별 일정 렌더링
-    scheduleList.map((schedule: any) => {
-      const start = format(schedule.startDateTime, "yyyy-MM-dd"); // 시작일
-      const end = format(schedule.endDateTime, "yyyy-MM-dd"); // 종료일
-      const startDate = new Date(schedule.startDateTime); // 시작일
-      const endDate = new Date(schedule.endDateTime); // 종료일
-      let currentDate = schedule.startDateTime; // 현재 날짜
-      // 시작일과 종료일이 다른 경우
-      if (start !== end) {
-        // 시작일부터 종료일까지 반복
-        while (currentDate <= endDate) {
-          // 시작한날의 의 끝을 currentEnd로 설정
-          let currentEnd = endOfWeek(currentDate, { weekStartsOn: 0 });
-          // currentEmd가 종료일보다 큰 경우 종료일로 설정
-          if (currentEnd > endDate) {
-            currentEnd = endDate;
-          }
-          // 시작일의 포맷
-          const startFormat = format(currentDate, "yyyy-MM-dd");
-          // 차이
-          const dayDiff = differenceInCalendarDays(currentEnd, currentDate);
-          // 2번째 시작부터 시작일이 배열에 없으면 추가
-          if (!dates[startFormat]) {
-            dates[startFormat] = [];
-          }
-          // 차이만큼 길이를 계산해서 추가
-          dates[startFormat].push(
-            <AllDaySchedule
-              width={dayDiff * 125 + 100}
-              key={`${schedule.id}-${startFormat}`}
-              title={schedule.name}
-            />
-          );
-          //하루 증가
-          currentDate = addDays(currentDate, 1);
-          // 첫날이후로 빈공간을 체우기 위한 SeparateSchedule 추가
-          while (currentDate <= currentEnd) {
-            // 배열에 없으면 추가
-            if (!dates[format(currentDate, "yyyy-MM-dd")]) {
-              dates[format(currentDate, "yyyy-MM-dd")] = [];
-            }
-            dates[format(currentDate, "yyyy-MM-dd")].push(
-              <SeparateSchedule view="hide" title={schedule.name} width={100}></SeparateSchedule>
-            );
-            currentDate = addDays(currentDate, 1);
-          }
+
+  // 일정별 날짜별 일정 렌더링
+  scheduleList.map((schedule: schedule) => {
+    if (schedule.type === "WORKING") return null;
+    const start = format(schedule.startDatetime, "yyyy-MM-dd"); // 시작일
+    const end = format(schedule.endDatetime, "yyyy-MM-dd"); // 종료일
+    const startDate = new Date(schedule.startDatetime); // 시작일
+    const endDate = new Date(schedule.endDatetime); // 종료일
+    // if (startDate < cStartDate || startDate > cEndDate) {
+    //   return null;
+    // }
+    let currentDate = schedule.startDatetime; // 현재 날짜
+    // 시작일과 종료일이 다른 경우
+    if (start !== end) {
+      // 시작일부터 종료일까지 반복
+      while (currentDate <= endDate) {
+        // 시작한날의 의 끝을 currentEnd로 설정
+        let currentEnd = endOfWeek(currentDate, { weekStartsOn: 0 });
+        // currentEmd가 종료일보다 큰 경우 종료일로 설정
+        if (currentEnd > endDate) {
+          currentEnd = endDate;
         }
-      } else {
-        // 시작일과 종료일이 같은 경우
-        // 종일일정인지 확인
-        const isAllDay =
-          schedule.startDateTime.getHours() === 0 && schedule.endDateTime.getHours() === 23;
-        dates[format(currentDate, "yyyy-MM-dd")].push(
-          isAllDay ? (
-            // 종일일정인 경우
-            <AllDaySchedule width={100} key={schedule.id} title={schedule.name} />
-          ) : (
-            // 종일일정이 아닌 경우
-            <PartialDaySchedule key={schedule.id} title={schedule.name} date={currentDate} />
-          )
+        // 시작일의 포맷
+        const startFormat = format(currentDate, "yyyy-MM-dd");
+        // 차이
+        const dayDiff = differenceInCalendarDays(currentEnd, currentDate);
+        // 2번째 시작부터 시작일이 배열에 없으면 추가
+        if (!dates[startFormat]) {
+          dates[startFormat] = [];
+        }
+        // 차이만큼 길이를 계산해서 추가
+        dates[startFormat].push(
+          <AllDaySchedule
+            triggerReload={triggerReload}
+            color={
+              schedule.color === 0
+                ? "blue"
+                : schedule.color === 1
+                ? "green"
+                : schedule.color === 2
+                ? "orange"
+                : schedule.color === 3
+                ? "yellow"
+                : "black50" || "blue"
+            }
+            endDatetime={format(schedule.endDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+            startDatetime={format(schedule.startDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+            scheduleId={schedule.scheduleId}
+            width={dayDiff * 100 + 80}
+            key={`${schedule.scheduleId}-${startFormat}`}
+            title={schedule.name}
+          />
         );
+        //하루 증가
+        currentDate = addDays(currentDate, 1);
+        // 첫날이후로 빈공간을 체우기 위한 SeparateSchedule 추가
+        while (currentDate <= currentEnd) {
+          // 배열에 없으면 추가
+          if (!dates[format(currentDate, "yyyy-MM-dd")]) {
+            dates[format(currentDate, "yyyy-MM-dd")] = [];
+          }
+          dates[format(currentDate, "yyyy-MM-dd")].push(
+            <SeparateSchedule
+              triggerReload={triggerReload}
+              color={
+                schedule.color === 0
+                  ? "blue"
+                  : schedule.color === 1
+                  ? "green"
+                  : schedule.color === 2
+                  ? "orange"
+                  : schedule.color === 3
+                  ? "yellow"
+                  : "black50" || "blue"
+              }
+              endDatetime={format(schedule.endDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+              startDatetime={format(schedule.startDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+              scheduleId={schedule.scheduleId}
+              view="hide"
+              title={schedule.name}
+              width={100}
+            ></SeparateSchedule>
+          );
+          currentDate = addDays(currentDate, 1);
+        }
       }
-      return null;
-    });
-  }
+    } else {
+      // 시작일과 종료일이 같은 경우
+      // 종일일정인지 확인
+      const isAllDay =
+        schedule.startDatetime.getHours() === 0 && schedule.endDatetime.getHours() === 23;
+      dates[format(currentDate, "yyyy-MM-dd")].push(
+        isAllDay ? (
+          // 종일일정인 경우
+          <AllDaySchedule
+            triggerReload={triggerReload}
+            color={
+              schedule.color === 0
+                ? "blue"
+                : schedule.color === 1
+                ? "green"
+                : schedule.color === 2
+                ? "orange"
+                : schedule.color === 3
+                ? "yellow"
+                : "black50" || "blue"
+            }
+            endDatetime={format(schedule.endDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+            startDatetime={format(schedule.startDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+            scheduleId={schedule.scheduleId}
+            width={80}
+            key={schedule.scheduleId}
+            title={schedule.name}
+          />
+        ) : (
+          // 종일일정이 아닌 경우
+          <PartialDaySchedule
+            triggerReload={triggerReload}
+            color={
+              schedule.color === 0
+                ? "blue"
+                : schedule.color === 1
+                ? "green"
+                : schedule.color === 2
+                ? "orange"
+                : schedule.color === 3
+                ? "yellow"
+                : "black50" || "blue"
+            }
+            startDatetime={format(schedule.startDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+            endDatetime={format(schedule.endDatetime, "yyyy-MM-dd'T'HH:mm:ss")}
+            scheduleId={schedule.scheduleId}
+            key={schedule.scheduleId}
+            title={schedule.name}
+            date={currentDate}
+          />
+        )
+      );
+    }
+    return null;
+  });
+
   return (
     <CalanderLayout>
       {/* 요일 */}
@@ -125,6 +208,7 @@ export function MonthViewCalendar({ selectedDate, scheduleList }: IMonthViewCale
               .slice(index, index + 7)
               .map((subDate, subIndex) => (
                 <DayForMonth
+                  triggerReload={triggerReload}
                   key={subDate}
                   // 날짜와 일정을 전달
                   day={{ date: new Date(subDate), schedules: dates[subDate] }}
