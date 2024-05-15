fafe50026203bfb52001f8f22957dd18ae42d02e
se2develop <seahee0130@gmail.com>
2024-05-16 02:09:10 +0900

    ✨Feat :  add myPage/notificationBox page
    Change-Id: Ib0deede44d56e9b12972926cada24100fdd10b33

---
+++ src/app/myPage/notificationBox/ui/created/createdBox.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+import { Color } from "@/shared/lib/styles/color";
+import NoticeLogo from "../noticeLogo";
+import NoticeInfo from "./noticeInfo";
+import { NoticeLogoProps } from "@/shared/lib/type";
+
+export default function CreatedBox({ data }: any) {
+  const [year, month, day] = data.startTime.split("T")[0].split("-");
+
+  const [logoData, setLogoData] = useState<NoticeLogoProps>({
+    type: data.type,
+    month: parseInt(month),
+    day: day,
+    date: new Date(data.startTime.split("T")[0]),
+    response: data.response ? data.response : "",
+  });
+
+  return (
+    <AlarmBoxDiv>
+      <NoticeLogo logoData={logoData} />
+      <NoticeInfo data={data} />
+    </AlarmBoxDiv>
+  );
+}
+const AlarmBoxDiv = styled.div`
+  border: 1px solid ${Color("black200")};
+  display: flex;
+  border-radius: 10px;
+  margin: 1rem;
+  padding: 1rem;
+  width: 55rem;
+  justify-content: center;
+`;
