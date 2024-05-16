cf4a42015d5f8eebecc8f4ce6f316028963e7161
card0923 <card0293@naver.com>
2024-05-16 09:36:58 +0900

    🐛Fix :  fix noto sans test
    Change-Id: I1b9cde331595afe4c6604c199e479588797eeb03

---
+++ src/__mocks__/next/font/google.js
@@ -0,0 +1,9 @@
+// __mocks__/@next/font/google.js
+module.exports = {
+  Noto_Sans_KR: () => ({
+    className: "mock-noto-sans-kr",
+    style: {
+      fontFamily: "Noto Sans KR, sans-serif",
+    },
+  }),
+};
