397a332e7675ba271a28ef995dff59a391debcc2
se2develop <seahee0130@gmail.com>
2024-05-17 12:58:54 +0900

    🚑Hotfix :  fix apiurl error
    Change-Id: Iefa40784ca7439e85a7b7ba3f1d0f0f8ec0e8d15

--- src/shared/api/postMeetingAccepted.tsx
+++ src/shared/api/postMeetingAccepted.tsx
@@ -3,8 +3,7 @@ import { fetchWithInterceptor } from "@/shared/index";
 export async function PostMeetingAccepted(scheduleId: number) {
   try {
     const res = await fetchWithInterceptor(
-      // `https://gateway.edgescheduler.co.kr/schedule-service/schedules/${scheduleId}/members/attendance`,
-      `https://gateway.edgescheduler.co.kr/schedule-service/schedules/89/members/attendance`,
+      `https://gateway.edgescheduler.co.kr/schedule-service/schedules/${scheduleId}/members/attendance`,
       {
         method: "POST",
         body: JSON.stringify({
